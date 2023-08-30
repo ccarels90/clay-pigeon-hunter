fe3c882db607c6b5cbb0c7f8a139c5866850d8b2
Carlo Carels <carlo@reachdigital.nl>
2023-08-30 15:32:10 +0200

    chore: create useIsMobile hook to determine if it should the mobile or desktop version

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,14 @@
 import styles from './page.module.css';
 import Controller from '@/components/Controller';
 import Playground from '@/components/Playground';
+import { useIsMobile } from '@/hooks/useIsMobile';
 
 export default function Home() {
+  const isMobileDevice = useIsMobile();
 
   return (
     <main className={styles.main}>
+      {isMobileDevice ? <Controller /> : <Playground />}
     </main>
   );
 }
