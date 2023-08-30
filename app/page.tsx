8e7f2dcac5af2a5fc36a1931e00f101865e2b49c
Carlo Carels <carlo@reachdigital.nl>
2023-08-30 15:30:54 +0200

    chore: create a homepage template

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+'use client';
+import styles from './page.module.css';
+import Controller from '@/components/Controller';
+import Playground from '@/components/Playground';
+
+export default function Home() {
+
+  return (
+    <main className={styles.main}>
+    </main>
+  );
+}
