4775fad3a37d8d2e4e7c6eaea54c9390974724a7
Carlo Carels <carlo@reachdigital.nl>
2023-08-30 15:27:14 +0200

    chore: install nextjs files

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import './globals.css';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'Clay Pigeon Hunter Game!',
+  description: 'Shoot the Clay!',
+};
+
+type RootLayoutProps = {
+  children: React.ReactNode;
+};
+
+export default function RootLayout({ children }: RootLayoutProps) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
