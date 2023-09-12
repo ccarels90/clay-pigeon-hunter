a36625abc3556c2b26107af437136c38524a801b
Carlo Carels <carlo@reachdigital.nl>
2023-09-12 22:07:45 +0200

    refactor: remove unused files

--- components/Playground.tsx
+++
@@ -1,9 +0,0 @@
-export default function Playground() {
-  return (
-    <div className="styles.playground">
-      <div className="styles.score">
-        <h1>Playground</h1>
-      </div>
-    </div>
-  );
-}
